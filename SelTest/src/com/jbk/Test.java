ae1477e0d2f3b6a18227081189d494d26d771765
jayshreedigrase <jayshreedigrase@jayshrees-air>
2020-06-02 16:52:24 +0530

    d

---
+++ SelTest/src/com/jbk/Test.java
@@ -0,0 +1,24 @@
+package com.jbk;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Test {
+	public static  WebDriver driver;
+	
+	
+public static void main(String[] args) {
+	/*System.out.println(System.getProperty("user.dir"));
+	System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/geckodriver");
+	DesiredCapabilities dc = new DesiredCapabilities();
+	dc.setCapability("marionatte", false);
+	FirefoxOptions opt = new FirefoxOptions();
+	opt.merge(dc);
+	driver=new FirefoxDriver();*/
+	
+	
+	
+}
+}
